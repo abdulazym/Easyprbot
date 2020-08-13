e5d98780980c4a1bac68236632390e6bcd0f4188
Dadaev Abdulazym <adadaev2016@gmail.com>
2020-08-13 16:20:56 +0300

    Сделал страницу наборы

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -18,13 +18,27 @@
         const mdBlockDay = manyDayTime.find('.select__header--days');
         const mdBlockTime = manyDayTime.find('.select__header--time');
         const mdErrorText = manyDayTime.find('.error__text');
+        // const manyDayTimeJS = document.querySelectorAll('.')
+        // const manyDayTimeJS = document.getElementById('manydaytime');
 
         // const itemText = item.text();
         // const textDays = ;
         // const textTime = "Время";
-        
-        console.log(manyDayTime.find('.select__current--days').hasClass('select__current--days-active'));
-        if(manyDayTime.find('.select__current--days').hasClass('select__current--days-active') && manyDayTime.find('.select__current--time').hasClass('select__current--time-active')){
+        // const currentDay = manyDayTimeJS.querySelectorAll('.select__current--days');
+        // console.log(currentDay);
+        // console.log(currentDay.hasClass('select__current--days-active'));
+        // const validDay = currentDay.each(function(){
+        //     currentDay.hasClass('select__current--days-active');
+        // });
+        // if(currentDay.every(el=>el.classList.contains('select__current--days-active'))){
+        //     console.log("good");
+        // }
+        // console.log(manyDayTime.find('.select__current--days').hasClass('select__current--days-active'));
+        console.log($('.select__current--days.select__current--days-active'));
+        if($('.select__current--days.select__current--days-active').length>=3){
+            console.log("well");
+        }
+        if(manyDayTime.find('.select__current--days.select__current--days-active').length>=3 && manyDayTime.find('.select__current--time.select__current--time-active').length>=3){
             if(!onlyTime.find('.select__current--time').hasClass('select__current--time-active')){
                 otBlockTime.addClass('error');
                 otErrorText.text('Заполните поля, чтобы продолжить');
@@ -95,3 +109,4 @@
     });
 
     // item.text() === "День недели" || itemTime.text() === "Время"
+    //manyDayTime.find('.select__current--days').hasClass('select__current--days-active') && manyDayTime.find('.select__current--time').hasClass('select__current--time-active'
