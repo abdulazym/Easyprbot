e833815225ef1ce030048dbee3613f0993a223af
Dadaev Abdulazym <adadaev2016@gmail.com>
2020-09-14 20:44:30 +0300

    Доделал модерацию

--- src/scripts/statisticsTabs.js
+++ src/scripts/statisticsTabs.js
@@ -1,24 +1,27 @@
+(function(){
+    const tabs = document.querySelectorAll('.moderation__tab--item');
+    const buttons = document.querySelectorAll('.btn__tabs');
+
+function change (target, index, buttons, tabs,  buttonActive, tabActive){
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('btn__tabs-active');
+        tabs[i].classList.remove('moderation__tab-active');
+    }
+    target.classList.add(buttonActive);
+    tabs[index].classList.add(tabActive);
+}
 
 
-// (function(){
-//     const tabs = document.querySelectorAll('.format__item');
-//     const button = document.querySelector('.btn--next--step');
-
-// function change (target, button, tabs, tabActive){
-
-//     for (let i = 0; i < button.length; i++) {
-//         // buttons[i].classList.remove('set-custom__btn-active');
-//         tabs[i].classList.remove('format__item-active');
-//     }
-//     // target.classList.add(buttonActive);
-//     tabs[index].classList.add(tabActive);
-// }
-
-
-//         button.addEventListener('click', (e)=>{
-//             e.preventDefault();
+    for (let index = 0; index < buttons.length; index++) {
+        const buttonElem = buttons[index];
+        
+        buttonElem.addEventListener('click', (e)=>{
+            e.preventDefault();
             
-//             change(e.currentTarget, button, tabs, 'format__item-active');
-//         });
+            change(e.currentTarget, index, buttons, tabs, 'btn__tabs-active', 'moderation__tab-active');
+        });
+    }
 
-// })();
+})();
+
