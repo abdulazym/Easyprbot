f96ce99051da7ffe4ef4003b779de301eead0417
Dadaev Abdulazym <adadaev2016@gmail.com>
2020-09-23 23:36:13 +0300

    Скрытая карточка

--- src/scripts/howFill.js
+++ src/scripts/howFill.js
@@ -6,13 +6,15 @@
             $('.statistics__format--header--mobile').addClass('statistics__none');
             $('.statistics__format--title').addClass('statistics__none');
             $(this).addClass('statistics__none');
+            $('.statistics__row').addClass('statistics__row--margin');
         });
 
-        $('.remove-add').click(function(e){
+        $('.btn__back--how').click(function(e){
             $('.statistics__how--none').removeClass('statistics__how');
             $('.statistics__format--header--mobile').removeClass('statistics__none');
             $('.statistics__format--title').removeClass('statistics__none');
             $('.textbluemobile').removeClass('statistics__none');
+            $('.statistics__row').removeClass('statistics__row--margin');
         });
     };
 
